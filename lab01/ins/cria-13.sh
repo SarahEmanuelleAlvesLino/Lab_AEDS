b52aeceb1dcdeef0c1d73f9e28b564ef94e847dc
Sarah Lino <156951487+SarahEmanuelleAlvesLino@users.noreply.github.com>
2024-08-29 08:57:32 -0300

    Lab01

---
+++ lab01/ins/cria-13.sh
@@ -0,0 +1,10 @@
+for ((i=10;i<10100;i=i+1)); do echo "insere_inicio $i"; echo "insere_fim $i" ; done
+for ((i=10;i<1100;i=i+1)); do echo "remove_inicio"; echo "remove_fim" ; done 
+for ((i=100;i<1100;i=i+1)); do echo "remove_i_esimo $i" ; done
+for ((i=100;i<200;i=i+1)); do echo "remove_inicio" ; done
+for ((i=100;i<200;i=i+1)); do echo "remove_fim" ; done
+echo "imprime"
+for ((i=1000111;i<2000100;i=i+1)); do echo "insere_inicio $i" ; done
+for ((i=10111;i<200111;i=i+100)); do echo "remove_i_esimo $i" ; done
+for ((i=1;i<20;i=i+1)); do echo "remove_fim" ; done
+echo "imprime"
